b932e570861507d35346e0ec39e6ddb5ff6553e3
Ilkin Mammadli <lester54539@gmail.com>
2022-03-28 01:13:02 +0400

    Add project files.

---
+++ View/MonefyView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Monefy.View
+{
+    /// <summary>
+    /// Interaction logic for MonefyView.xaml
+    /// </summary>
+    public partial class MonefyView : Window
+    {
+        public MonefyView()
+        {
+            InitializeComponent();
+        }
+    }
+}
